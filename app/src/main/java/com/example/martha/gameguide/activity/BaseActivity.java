e63bb5c170308c5161cf0f868be7c7f921bff448
ginosian <marta.ginosian@gmail.com>
2016-05-22 15:24:15 +0400

    Some changes.

--- app/src/main/java/com/example/martha/gameguide/activity/BaseActivity.java
+++ app/src/main/java/com/example/martha/gameguide/activity/BaseActivity.java
@@ -6,7 +6,6 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -48,7 +47,6 @@ public abstract class BaseActivity extends AppCompatActivity {
     protected void initDrawer() {
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         mDrawerList = (ListView) findViewById(R.id.left_drawer);
-        mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
         mDrawerList.setAdapter(new ArrayAdapter<>(this, R.layout.drawer_list_item_example, mItemTitles));
     }
 
